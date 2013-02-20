aab7a5de37ca39d338b331dc03107515403fbe2a
Marko Sundic <msundic@gmail.com>
2013-02-20 22:59:37 +0100

    first run

---
+++ src/tutorials/HelloWorld.java
@@ -0,0 +1,15 @@
+package tutorials;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marko
+ * Date: 20.02.13
+ * Time: 22:54
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+
+}
