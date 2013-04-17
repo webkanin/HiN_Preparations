e6be5a967db711326d72a6fe4459e11bc548f7e5
Marko Sundic <msundic@gmail.com>
2013-04-17 22:55:30 +0200

    changed the readme file

--- src/tutorials/HelloWorld.java
+++ src/tutorials/HelloWorld.java
@@ -10,6 +10,10 @@ public class HelloWorld {
 
     public static void main(String args[]){
         System.out.println("Hello World!");
+        System.out.println("Lol");
+        System.out.println("What?");
+        System.out.print("haha");
+
     }
 
 }
